97757876f02dfe19d76b412cb0a633a211c7d3ce
rpr2911 <Raihan291101>
2019-07-24 09:21:27 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tablelayout/Main32Activity.java
@@ -0,0 +1,14 @@
+package com.example.tablelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main32Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main32);
+    }
+}
